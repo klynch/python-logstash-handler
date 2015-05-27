7f83888c957b892e6cc9d2e92f49a2737a9eabfe
andreydroid <andrey.mak@gmail.com>
2015-05-27 16:03:36 +0300

    Add support for CA certificates
    better SSL support

--- logstash_handler/__init__.py
+++ logstash_handler/__init__.py
@@ -6,17 +6,18 @@ class LogstashHandler(SocketHandler):
   Sends output to an optionally encrypted streaming logstash TCP listener.
   """
 
-  def __init__(self, host, port, keyfile=None, certfile=None, ssl=True):
+  def __init__(self, host, port, keyfile=None, certfile=None, ca_certs=None, ssl=True):
     SocketHandler.__init__(self, host, port)
     self.keyfile = keyfile
     self.certfile = certfile
+    self.ca_certs = ca_certs
     self.ssl = ssl
 
 
   def makeSocket(self, timeout=1):
     s = SocketHandler.makeSocket(self, timeout)
     if self.ssl:
-      return ssl.wrap_socket(s, keyfile=self.keyfile, certfile=self.certfile)
+      return ssl.wrap_socket(s, keyfile=self.keyfile, certfile=self.certfile, ca_certs=self.ca_certs)
     return s
 
 
